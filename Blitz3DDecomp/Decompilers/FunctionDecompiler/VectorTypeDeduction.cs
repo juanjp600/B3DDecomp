d985e7e0947e1e6627b905d660a3b8763d8d1c40
Juan Pablo Arce <juanjprot@gmail.com>
2023-10-22 19:36:05 -0300

    Formatting for readability

--- Blitz3DDecomp/Decompilers/FunctionDecompiler/VectorTypeDeduction.cs
+++ Blitz3DDecomp/Decompilers/FunctionDecompiler/VectorTypeDeduction.cs
@@ -7,12 +7,20 @@ static class VectorTypeDeduction
         for (int i = 2; i < section.Instructions.Count - 1; i++)
         {
             var instruction = section.Instructions[i];
-            if (instruction.Name != "call" || !instruction.LeftArg.Contains("_builtIn__bbVecAlloc", StringComparison.Ordinal)) { continue; }
+            if (instruction.Name != "call"
+                || !instruction.LeftArg.Contains("_builtIn__bbVecAlloc", StringComparison.Ordinal))
+            {
+                continue;
+            }
+
             var vecTypeToRegister = section.Instructions[i - 2];
             var vecType = DeclType.FromDesc(vecTypeToRegister.RightArg[1..]);
+
             var registerToArg = section.Instructions[i - 1];
+
             var resultToVariable = section.Instructions[i + 1];
             var variable = function.InstructionArgumentToVariable(resultToVariable.LeftArg);
+
             if (vecTypeToRegister.Name == "mov"
                 && vecType.IsArrayType
                 && vecTypeToRegister.LeftArg == registerToArg.RightArg

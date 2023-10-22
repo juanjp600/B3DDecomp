c2c39ceab8cfee244adfc822d326df532b2cc614
Juan Pablo Arce <juanjprot@gmail.com>
2023-10-22 16:07:48 -0300

    Massively simplified CountLocals, plus it probably works better than whatever I came up with before

--- Blitz3DDecomp/Decompilers/FunctionDecompiler/CountLocals.cs
+++ Blitz3DDecomp/Decompilers/FunctionDecompiler/CountLocals.cs
@@ -11,43 +11,18 @@ static partial class FunctionDecompiler
         public static void Process(Function function)
         {
             if (!function.AssemblySections.Any()) { return; }
-            var coreSectionInstructions =
-                function.CoreSymbolName == "__MAIN"
-                    ? function.AssemblySections.First(kvp => kvp.Key.EndsWith("_begin__MAIN")).Value.Instructions.ToArray()
-                    : function.AssemblySections[function.CoreSymbolName].Instructions.Skip(5).ToArray();
-
-            var ebpOffsetLocalRegex = new Regex("\\[ebp-0x([0-9a-f]+)\\]");
-            var ebpOffsetArgRegex = new Regex("\\[ebp\\+0x([0-9a-f]+)\\]");
-            var ebpOffsets = new HashSet<int>();
-            foreach (var instruction in coreSectionInstructions)
-            {
-                if (ebpOffsetArgRegex.Match(instruction.LeftArg) is { Success: true })
-                {
-                    break;
-                }
-                if (instruction.Name == "mov" && ebpOffsetLocalRegex.Match(instruction.LeftArg) is { Success: true } ebpOffsetMatch)
-                {
-                    if (!ebpOffsets.Add(int.Parse(ebpOffsetMatch.Groups[1].Value, NumberStyles.HexNumber)))
-                    {
-                        break;
-                    }
-                    continue;
-                }
-
-                if (instruction.LeftArg.ContainsRegister() && instruction.LeftArg.Contains("ebp")) { break; }
-                if (instruction.RightArg.ContainsRegister() && instruction.RightArg.Contains("ebp")) { break; }
-                if (instruction.LeftArg.Contains("@_v") || instruction.RightArg.Contains("@_v")) { break; }
-                if (instruction.LeftArg.Contains("@_f") || instruction.RightArg.Contains("@_f")) { break; }
-            }
-
-            for (int i = 4; i <= ebpOffsets.Count * 4; i += 4)
-            {
-                if (!ebpOffsets.Contains(i))
-                {
-                    Debugger.Break();
-                }
-            }
-            function.LocalVariables.AddRange(Enumerable.Range(0, ebpOffsets.Count).Select(i => new Function.LocalVariable($"local{i}") { DeclType = DeclType.Unknown }));
+            var lastLocalIndex = function.AssemblySections.Values
+                .SelectMany(s => s.Instructions)
+                .SelectMany(i => new[] { i.LeftArg, i.RightArg })
+                .Select(a => a.StripDeref())
+                .Where(a => a.StartsWith("ebp-0x", StringComparison.Ordinal))
+                .Distinct()
+                .Select(a => int.Parse(a[6..], NumberStyles.HexNumber) >> 2)
+                .Append(0)
+                .Max();
+            function.LocalVariables.AddRange(
+                Enumerable.Range(0, lastLocalIndex)
+                .Select(i => new Function.LocalVariable($"local{i}")));
         }
     }
 }

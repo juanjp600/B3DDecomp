8f185d57fa18d5b7922bd4f69a71b8ab3c7674d0
Juan Pablo Arce <juanjproT@gmail.com>
2024-01-05 11:10:15 -0300

    Added BbArrayAccessRewrite

---
+++ Blitz3DDecomp/DecompilerSteps/Step3/BbArrayAccessRewrite.cs
@@ -0,0 +1,42 @@
+﻿using B3DDecompUtils;
+
+namespace Blitz3DDecomp.DecompilerSteps.Step3;
+
+static class BbArrayAccessRewrite
+{
+    private static bool ProcessSection(Function.AssemblySection section)
+    {
+        bool somethingChanged = false;
+        for (int i = 0; i < section.Instructions.Count; i++)
+        {
+            var instruction = section.Instructions[i];
+            if (instruction.Name != "add"
+                || !instruction.DestArg[..3].IsRegister())
+            {
+                continue;
+            }
+
+            if (section.Owner.InstructionArgumentToVariable(instruction.SrcArg1) is not { } array
+                || !array.DeclType.IsArrayType)
+            {
+                continue;
+            }
+
+            instruction.Name = "mov";
+            instruction.SrcArg1 =  $"{array.Name}[{instruction.DestArg}>>2]";
+            Logger.WriteLine($"{section.Owner}: {array.Name} access at {i}");
+            somethingChanged = true;
+        }
+        return somethingChanged;
+    }
+
+    public static bool Process(Function function)
+    {
+        bool somethingChanged = false;
+        foreach (var section in function.AssemblySections.Values)
+        {
+            somethingChanged |= ProcessSection(section);
+        }
+        return somethingChanged;
+    }
+}

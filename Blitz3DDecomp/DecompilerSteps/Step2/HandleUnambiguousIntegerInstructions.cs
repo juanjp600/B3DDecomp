6b470ee6a5810de0cfa3073413dad10a8b0a416a
Juan Pablo Arce <juanjproT@gmail.com>
2024-01-07 09:56:00 -0300

    Handle some instructions that can only apply to integers

---
+++ Blitz3DDecomp/DecompilerSteps/Step2/HandleUnambiguousIntegerInstructions.cs
@@ -0,0 +1,35 @@
+﻿using B3DDecompUtils;
+
+namespace Blitz3DDecomp.DecompilerSteps.Step2;
+
+static class HandleUnambiguousIntegerInstructions
+{
+    private static void ProcessSection(Function.AssemblySection section)
+    {
+        foreach (var instruction in section.Instructions)
+        {
+            if (instruction.Name is not ("shl" or "sar")) { continue; }
+
+            var destVar = section.Owner.InstructionArgumentToVariable(instruction.DestArg);
+            var srcVar = section.Owner.InstructionArgumentToVariable(instruction.SrcArg1);
+
+            void trySetVarToInt(Variable? variable)
+            {
+                if (variable?.DeclType != DeclType.Unknown) { return; }
+                variable.DeclType = DeclType.Int;
+                Logger.WriteLine($"{section.Owner}: {variable.Name} is {DeclType.Int} because {instruction}");
+            }
+
+            trySetVarToInt(destVar);
+            trySetVarToInt(srcVar);
+        }
+    }
+
+    public static void Process(Function function)
+    {
+        foreach (var section in function.AssemblySections.Values)
+        {
+            ProcessSection(section);
+        }
+    }
+}
---
+++ Blitz3DDecomp/DecompilerSteps/Step3/HandleIntegerAddAndSub.cs
@@ -0,0 +1,45 @@
+﻿using B3DDecompUtils;
+
+namespace Blitz3DDecomp.DecompilerSteps.Step3;
+
+static class HandleIntegerAddAndSub
+{
+    private static bool ProcessSection(Function.AssemblySection section)
+    {
+        bool somethingChanged = false;
+        foreach (var instruction in section.Instructions)
+        {
+            if (instruction.Name is not ("add" or "sub")) { continue; }
+
+            var destVar = section.Owner.InstructionArgumentToVariable(instruction.DestArg);
+            var src1Var = section.Owner.InstructionArgumentToVariable(instruction.SrcArg1);
+            var src2Var = section.Owner.InstructionArgumentToVariable(instruction.SrcArg2);
+
+            void trySetVarToInt(Variable? variable)
+            {
+                if (variable?.DeclType != DeclType.Unknown) { return; }
+                variable.DeclType = DeclType.Int;
+                Logger.WriteLine($"{section.Owner}: {variable.Name} is {DeclType.Int} because {instruction}");
+                somethingChanged = true;
+            }
+
+            if (destVar?.DeclType == DeclType.Int || src1Var?.DeclType == DeclType.Int)
+            {
+                trySetVarToInt(destVar);
+                trySetVarToInt(src1Var);
+                trySetVarToInt(src2Var);
+            }
+        }
+        return somethingChanged;
+    }
+
+    public static bool Process(Function function)
+    {
+        bool somethingChanged = false;
+        foreach (var section in function.AssemblySections.Values)
+        {
+            somethingChanged |= ProcessSection(section);
+        }
+        return somethingChanged;
+    }
+}

243fb9a23505a6f4c7e097f58f863ec1a7a3ef96
Juan Pablo Arce <juanjproT@gmail.com>
2024-01-13 22:06:29 -0300

    This is why QA matters

--- Blitz3DDecomp/Basics/Function/Blitz3dTssBuiltIns.cs
+++ Blitz3DDecomp/Basics/Function/Blitz3dTssBuiltIns.cs
@@ -5,7 +5,7 @@ static class Blitz3dTssBuiltIns
     public static void Init()
     {
         Blitz3dBuiltIns.Init();
-        var loadFont = Function.GetFunctionByName("_builtIn__floadfont");
+        var loadFont = Function.GetFunctionByName("_builtIn_floadfont");
         loadFont.Parameters.RemoveRange(2, 3);
     }
 }

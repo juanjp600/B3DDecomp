d3317328ae57ca7269ccad2528a7e0974a48482e
Juan Pablo Arce <juanjproT@gmail.com>
2024-01-13 22:06:06 -0300

    DataReadStatement

---
+++ Blitz3DDecomp/Basics/Function/HighLevel/Statement/DataSection/DataReadStatement.cs
@@ -0,0 +1,16 @@
+﻿namespace Blitz3DDecomp.HighLevel;
+
+sealed record DataReadStatement(AccessExpression Destination) : Statement
+{
+    public override string StringRepresentation
+        => $"Read {Destination.StringRepresentation}";
+
+    public override IEnumerable<Expression> InnerExpressions { get; } = new[] { Destination };
+
+    protected override Statement MapImplementation(Func<Statement, Statement> statementMapper, Func<Expression, Expression> expressionMapper)
+    {
+        return statementMapper(new DataReadStatement(
+            Destination.Map(expressionMapper) as AccessExpression
+                ?? throw new Exception("expressionMapper did not return an AccessExpression")));
+    }
+}

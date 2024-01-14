54bf5fd6e59241400d863425908f609b754d5b17
Juan Pablo Arce <juanjproT@gmail.com>
2024-01-13 22:05:45 -0300

    Added Expressions for comparisons between two different expressions

---
+++ Blitz3DDecomp/Basics/Function/HighLevel/Expression/Arithmetic/ExponentiationExpression.cs
@@ -0,0 +1,14 @@
+﻿namespace Blitz3DDecomp.HighLevel;
+
+sealed record ExponentiationExpression(Expression Base, Expression Exponent) : Expression
+{
+    public override string StringRepresentation
+        => $"({Base.StringRepresentation} ^ {Exponent.StringRepresentation})";
+
+    public override Expression Map(Func<Expression, Expression> mapper)
+    {
+        return mapper(new ExponentiationExpression(Base.Map(mapper), Exponent.Map(mapper)));
+    }
+
+    public override IEnumerable<Expression> InnerExpressions { get; } = new[] { Base, Exponent };
+}
---
+++ Blitz3DDecomp/Basics/Function/HighLevel/Expression/ComparisonResults/CompareToOther/OneIfExpressionIsGreaterThanOrEqualToOtherExpression.cs
@@ -0,0 +1,14 @@
+﻿namespace Blitz3DDecomp.HighLevel;
+
+sealed record OneIfExpressionIsGreaterThanOrEqualToOtherExpression(Expression Lhs, Expression Rhs) : Expression
+{
+    public override string StringRepresentation
+        => $"({Lhs.StringRepresentation} >= {Rhs.StringRepresentation})";
+
+    public override Expression Map(Func<Expression, Expression> mapper)
+    {
+        return mapper(new OneIfExpressionsEqualExpression(Lhs.Map(mapper), Rhs.Map(mapper)));
+    }
+
+    public override IEnumerable<Expression> InnerExpressions { get; } = new[] { Lhs, Rhs };
+}

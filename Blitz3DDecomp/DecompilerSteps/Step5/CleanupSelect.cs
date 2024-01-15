a2debf313cfa2a2bbdf65150490a7498413e8a84
Juan Pablo Arce <juanjproT@gmail.com>
2024-01-15 04:35:52 -0300

    Shift Case statements into their own sections to preserve goto semantics

--- Blitz3DDecomp/DecompilerSteps/Step5/CleanupSelect.cs
+++ Blitz3DDecomp/DecompilerSteps/Step5/CleanupSelect.cs
@@ -44,6 +44,8 @@ static class CleanupSelect
                 }
                 var selectInnerExpression = getSelectExpression(firstCondition);
 
+                // Match each section that's jumped to
+                // to the expressions representing the cases
                 var cases = new Dictionary<string, List<Expression>>();
                 foreach (var selectCase in chain)
                 {
@@ -69,6 +71,8 @@ static class CleanupSelect
                 var firstCaseStatementIndex = firstCaseSection.StartIndex;
                 if (firstCaseStatementIndex < startSection.StartIndex) { Debugger.Break(); }
 
+                // Generate new sections for the default case,
+                // because that's first in assembly but last in high level code
                 var tempSections = new List<TempSection>
                 {
                     new TempSection($"_endSelect{currentSectionNumber}", new List<Statement>())
@@ -90,19 +94,38 @@ static class CleanupSelect
                     tempSection.Statements.Add(function.HighLevelStatements[j]);
                 }
 
-                var statementToRemoveEverywhere = tempSections.Last().Statements.Last() as UnconditionalJumpStatement;
+                // Every case ends with an unconditional jump to the same place
+                // so we'll get it from the default case if it exists
+                var statementToRemoveEverywhere = tempSections.SelectMany(s => s.Statements).Last() as UnconditionalJumpStatement;
                 if (statementToRemoveEverywhere is null)
                 {
-                    Debugger.Break();
+                    //Debugger.Break();
                     return;
                 }
 
+                // Check that all cases actually have this same unconditional jump
+                for (var j = 0; j < allCaseSections.Length; j++)
+                {
+                    var caseSection = allCaseSections[j];
+                    var nextSection = j < allCaseSections.Length - 1 ? allCaseSections[j + 1] : sectionsByName[statementToRemoveEverywhere.SectionName];
+
+                    int lastStatementOfCase = nextSection.StartIndex - 1;
+                    function.FindSectionForStatementIndex(lastStatementOfCase, out var lastSectionOfCase, out _);
+                    if (lastSectionOfCase.Statements[^1] != statementToRemoveEverywhere)
+                    {
+                        Debugger.Break();
+                        return;
+                    }
+                }
+
+                // Check that the last case actually comes before the end of the Select statement block
                 var lastCaseSection = cases.Keys.Select(key => sectionsByName[key]).MaxBy(s => s.StartIndex)!;
                 if (sectionsByName[statementToRemoveEverywhere.SectionName].StartIndex < lastCaseSection.StartIndex)
                 {
                     Debugger.Break();
                 }
 
+                // Remove gotos
                 var chainStartIndexInStartSection = chainStart - startSection.StartIndex;
                 int startSectionSize = startSection.Statements.Count;
                 for (int j = chainStartIndexInStartSection; j < startSectionSize; j++)
@@ -126,9 +149,10 @@ static class CleanupSelect
                     var caseSection = allCaseSections[j];
                     if (j > 0) { cleanUpGotoOfPrevSection(caseSection); }
 
-                    caseSection.Statements.Insert(0, new CaseStatement(cases[caseSection.Name].ToArray()));
+                    //caseSection.Statements.Insert(0, new CaseStatement(cases[caseSection.Name].ToArray()));
                 }
 
+                // Remove old default case sections
                 foreach (var tempSection in tempSections.Skip(1).Reverse())
                 {
                     var oldSection = sectionsByName[tempSection.Name];
@@ -140,6 +164,7 @@ static class CleanupSelect
                     function.HighLevelSections.RemoveAt(oldSectionIndex);
                 }
 
+                // Regenerate default case sections and insert End Select
                 var indexToInsertDefault = function.HighLevelSections.IndexOf(sectionsByName[statementToRemoveEverywhere.SectionName]);
                 foreach (var tempSection in tempSections)
                 {
@@ -169,6 +194,21 @@ static class CleanupSelect
                 }
 
                 startSection.Statements.Add(new SelectStatement(selectInnerExpression));
+
+                // Insert Case statements in a section that comes before the section pointed to by goto
+                for (var j = 0; j < allCaseSections.Length; j++)
+                {
+                    var caseSection = allCaseSections[j];
+                    var caseSectionIndex = function.HighLevelSections.IndexOf(caseSection);
+
+                    var newSection = new HighLevelSection(function, $"_selectCase{currentSectionNumber}");
+                    currentSectionNumber++;
+                    function.HighLevelSections.Insert(caseSectionIndex, newSection);
+
+                    var caseStatement = new CaseStatement(cases[caseSection.Name].ToArray());
+                    newSection.Statements.Add(caseStatement);
+                    if (function.HighLevelStatements[newSection.StartIndex] != caseStatement) { Debugger.Break(); }
+                }
 
                 if (function.HighLevelStatements.Count(stmt => stmt is SelectStatement)
                     != function.HighLevelStatements.Count(stmt => stmt is EndSelectStatement))
@@ -219,6 +259,30 @@ static class CleanupSelect
                     handleChain(ref i);
                     continue;
                 }
+            }
+        }
+
+        // Push Case statements above every empty section that's next to them
+        // so remaining gotos can still work.
+        // Assembly sections are checked for emptiness because that's representative
+        // of the real semantics of the program, some non-empty sections may decompile
+        // down to no statements at all.
+        for (int i = 0; i < function.HighLevelSections.Count; i++)
+        {
+            var section = function.HighLevelSections[i];
+            if (section.Statements.Count != 1) { continue; }
+            if (section.Statements[0] is not CaseStatement) { continue; }
+
+            var reinsertIndex = i;
+            while (!function.AssemblySectionsByName.TryGetValue(function.HighLevelSections[reinsertIndex - 1].Name, out var assemblySection)
+                   || assemblySection.Instructions.Length == 0)
+            {
+                reinsertIndex--;
+            }
+            if (reinsertIndex < i)
+            {
+                function.HighLevelSections.RemoveAt(i);
+                function.HighLevelSections.Insert(reinsertIndex, section);
             }
         }
     }

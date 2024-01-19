5fb28d4809118c7667e0cbf9f395bc800b22224d
Juan Pablo Arce <juanjproT@gmail.com>
2024-01-19 10:52:36 -0300

    Added CleanupElseIf

---
+++ Blitz3DDecomp/DecompilerSteps/Step5/CleanupElseIf.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Blitz3DDecomp.HighLevel;
+
+namespace Blitz3DDecomp.DecompilerSteps.Step5;
+
+static class CleanupElseIf
+{
+    public static void Process(Function function)
+    {
+        var sectionsByName = function.HighLevelSectionsByName;
+        for (int i = 0; i < function.HighLevelStatements.Count - 1; i++)
+        {
+            if (function.HighLevelStatements[i] is not ElseStatement
+                || function.HighLevelStatements[i + 1] is not IfStatement ifStatement) { continue; }
+            int indent = 0;
+            int j = i + 1;
+            for (; j < function.HighLevelStatements.Count; j++)
+            {
+                indent -= function.HighLevelStatements[j].IndentationToSubtract;
+                if (indent < 0) { break; }
+                indent += function.HighLevelStatements[j].IndentationToAdd;
+            }
+            if (function.HighLevelStatements[j] is not EndIfStatement
+                || function.HighLevelStatements[j - 1] is not EndIfStatement) { continue; }
+
+            function.FindSectionForStatementIndex(j, out var endIfSection, out var indexInEndIfSection);
+            endIfSection.Statements.RemoveAt(indexInEndIfSection);
+            function.FindSectionForStatementIndex(i, out var elseIfSection, out var indexInElseIfSection);
+            elseIfSection.Statements.RemoveAt(indexInElseIfSection);
+            elseIfSection.Statements[indexInElseIfSection] = new ElseIfStatement(ifStatement.Condition);
+        }
+    }
+}
